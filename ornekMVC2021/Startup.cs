7af710c3e4044d435fcd047d34db21beebc69b02
Tarik Bagriyanik <tbagriyanik@gmail.com>
2021-01-25 22:51:13 +0300

    Add project files.

---
+++ ornekMVC2021/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ornekMVC2021.Startup))]
+namespace ornekMVC2021
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
